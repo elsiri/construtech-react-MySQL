86d6ae30102c247497554b7f20aeeccb12c3781d
elsiri <kevincastrillon31@gmail.com>
2023-07-04 20:44:37 -0500

    Se realizaron modificaciones
    Se agrego filtro para clientes
    Se agrego edit y add clientes
    Se agrego partials para reutilizar formulario de clientes

--- server/routes/clientes.routes.js
+++ server/routes/clientes.routes.js
@@ -24,22 +24,53 @@ router.get('/clientNew', (req,res) => {
     res.render('addCliente');
 });
 
+router.get('/clientEdit/:id', async (req,res) => {
+    try {
+        const [result] = await pool.query("SELECT * FROM cliente WHERE idCli = ?", [req.params.id])
+        if (result.length === 0) {
+            console.log("Error")
+            const [clientes] = await pool.query('SELECT *FROM cliente ORDER BY idCli DESC')
+            res.render('clientes', {message:{type:0,content:`No se encontro cliente con id: ${req.params.id}`}, result: clientes})
+        } else {
+            console.log(result)
+            res.render('editCliente', {result});
+        }
+        // console.log(result)
+        // res.render('editCliente', {result});        
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+
+});
+
 router.get("/clientsList", async (req,res) =>{
     try {
         const [result] = await pool.query('SELECT *FROM cliente ORDER BY idCli DESC')
         // console.log(result)
-        res.render("clientes", {result})        
+        res.render("clientes", {result: result,message:{type:2}})        
     } catch (error) {
         return res.status(500).json({message: error.message})
     }   
+})
+
+router.post("/clientsList", async (req,res) =>{
+    try {
+        const {nombre, estado} = req.body
+        const [result] = await pool.query("SELECT * FROM cliente WHERE nombre LIKE ?", ["%"+nombre+"%"])
+        console.log(result)
+        res.render("clientes", {result: result,message:{type:2}})                
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 router.post("/clientNew", async (req,res) =>{
     try {
         const {nombre, email, direccion, telefono, cedula, fecha_nac, estado} = req.body
         const [result] = await pool.query('INSERT INTO cliente SET nombre = ?, email = ?, direccion = ?, telefono = ?, cedula = ?, fecha_nac = ?, estado = ?',[nombre, email, direccion, telefono, cedula, fecha_nac, estado])
+        const [clientes] = await pool.query('SELECT *FROM cliente ORDER BY idCli DESC')        
         if (result.insertId) {
-            res.render("addCliente");
+            res.render("clientes", {result:clientes,message:{type:1,content:`Cliente agregado con exito!`}});
             console.log(
                 {
                     id:"Id insertado:"+result.insertId,
@@ -53,7 +84,23 @@ router.post("/clientNew", async (req,res) =>{
                 }
             )            
         } else {
-            console.log(result)
+            res.render("clientes", {message:`Error al intentar agregar cliente!`});
+        }
+    } catch (error) {
+        return res.status(500).json({message: error.message})        
+    }
+})
+
+router.post("/clientEdit", async (req,res) =>{
+    try {
+        const {idCli, nombre, email, direccion, telefono, cedula, fecha_nac, estado} = req.body
+        const [result] = await pool.query('UPDATE cliente SET nombre = ?, email = ?, direccion = ?, telefono = ?, cedula = ?, fecha_nac = ?, estado = ? WHERE idCli = ?',[nombre, email, direccion, telefono, cedula, fecha_nac, estado, idCli])
+        // console.log(result.affectedRows > 0)
+        if (result.affectedRows > 0) {
+            const [clientes] = await pool.query('SELECT *FROM cliente ORDER BY idCli DESC')                    
+            res.render("clientes", {result:clientes,message:{type:1,content:`Cliente editado con exito! id afectado: ${result.insertId}`}});          
+        } else {
+            res.render("clientes", {result:clientes,message:{type:1,content:`Error al intentar editar cliente: ${result.insertId}`}});
         }
     } catch (error) {
         return res.status(500).json({message: error.message})        

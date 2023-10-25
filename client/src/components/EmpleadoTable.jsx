d3e7fd8eb350a99086add080d5b9a4ab68aac01b
kevincastrillon70 <kevincastrillon31@gmail.com>
2023-10-25 12:33:36 -0500

    Se modifico interfaz para mostrar los empleados

---
+++ client/src/components/EmpleadoTable.jsx
@@ -0,0 +1,111 @@
+import { useEmpleados } from "../context/EmpleadosProvider";
+import { useNavigate } from "react-router-dom";
+import { Card, Typography } from "@material-tailwind/react";
+ 
+const TABLE_HEAD = ["Name", "Job", "Employed", "Accion"];
+ 
+const TABLE_ROWS = [
+  {
+    name: "John Michael",
+    job: "Manager",
+    date: "23/04/18",
+  },
+  {
+    name: "Alexa Liras",
+    job: "Developer",
+    date: "23/04/18",
+  },
+  {
+    name: "Laurent Perrier",
+    job: "Executive",
+    date: "19/09/17",
+  },
+  {
+    name: "Michael Levi",
+    job: "Developer",
+    date: "24/12/08",
+  },
+  {
+    name: "Richard Gran",
+    job: "Manager",
+    date: "04/10/21",
+  },
+];
+ 
+export default function EmpleadoTable({empleados}) {
+  return (
+    <Card className="h-full w-full overflow-scroll">
+      <table className="w-full min-w-max table-auto text-left">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head) => (
+              <th
+                key={head}
+                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+              >
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  {head}
+                </Typography>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {empleados.map(({ idEmp, nombre, cedula, email }, index) => {
+            const isLast = index === TABLE_ROWS.length - 1;
+            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+ 
+            return (
+              <tr key={idEmp}>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {nombre}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {cedula}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {email}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    as="a"
+                    href="#"
+                    variant="small"
+                    color="blue-gray"
+                    className="font-medium"
+                  >
+                <button className="bg-slate-800 px-2 py-1 text-white" onClick={ ()=> navigate(`/editarEmpleado/${empleado.idEmp}`)}>
+                    Editar
+                </button> 
+                  </Typography>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Card>
+  );
+}
--- client/src/pages/EmpleadosPage.jsx
+++ client/src/pages/EmpleadosPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import EmpleadoCard from "../components/EmpleadoCard";
+import EmpleadoTable from "../components/EmpleadoTable";
 import { useEmpleados } from "../context/EmpleadosProvider";
 function EmpleadosPage() {
 
@@ -15,7 +15,7 @@ function EmpleadosPage() {
             return <h1>Sin Empleados</h1>
             
         }
-        return empleados.map((empleado) =><EmpleadoCard empleado={empleado} key={empleado.idEmp} />)
+        return <EmpleadoTable empleados={empleados}/>
     }
 
     return(
@@ -24,7 +24,7 @@ function EmpleadosPage() {
                 <button className="block my-3 rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={ ()=> navigate(`/agregarEmpleado`)}>
                     Agregar empleado
                 </button>
-            <div className="grid grid-cols-3 gap-2">
+            <div className="grid">
                 {renderMain()}
             </div>
 
